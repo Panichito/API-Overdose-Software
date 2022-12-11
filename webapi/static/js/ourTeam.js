75ecdc2f87a9d3deef4649c23f74cb23f46bddef
Panichito <panithi2001big@gmail.com>
2022-12-11 16:31:53 +0700

    web favicon

--- webapi/static/js/ourTeam.js
+++ webapi/static/js/ourTeam.js
@@ -4,7 +4,7 @@ import memberProfile from "./memberProfile.js";
 
 let memberName1 = 'Panithi S.';
 let memberRole1 = 'Chief Executive Officer';
-let memberInfo1 = 'CPE @KMUTT Alumni';
+let memberInfo1 = 'ZEN Apprentice | Stay calm no panic';
 let memberImg1 = big;
 let facebookLink1 = 'https://www.facebook.com/panithi.suwanno';
 let twitterLink1 = 'https://github.com/Panichito';
@@ -12,7 +12,7 @@ let linkedinLink1 = 'https://www.linkedin.com/in/panithi-suwanno-89171a1a3/';
 
 let memberName2 = 'Kantawat S.';
 let memberRole2 = 'Chief Technology Officer';
-let memberInfo2 = '3th year student KMUTT';
+let memberInfo2 = 'Frontend Developer';
 let memberImg2 = map;
 let facebookLink2 = 'https://www.facebook.com/map.kantawat';
 let twitterLink2 = 'https://github.com/Miran-Mirantee';
@@ -20,7 +20,7 @@ let linkedinLink2 = 'javascript:;';
 
 let memberName3 = 'Phoramint T.';
 let memberRole3 = 'Chief Marketing Officer ';
-let memberInfo3 = '3th year student KMUTT';
+let memberInfo3 = 'Quality Assurance | Web Master';
 let memberImg3 = min;
 let facebookLink3 = 'https://www.facebook.com/profile.php?id=100007113468193';
 let twitterLink3 = 'https://github.com/PhoramintTaweeros';
@@ -28,7 +28,7 @@ let linkedinLink3 = 'javascript:;';
 
 let memberName4 = 'Poonyavee W.';
 let memberRole4 = 'Chief Data Officer';
-let memberInfo4 = '3th year student KMUTT';
+let memberInfo4 = 'Frontend Developer | UX UI Designer';
 let memberImg4 = pup;
 let facebookLink4 = 'https://www.facebook.com/poonyavee.wongwisetsuk';
 let twitterLink4 = 'https://github.com/pupshi';
@@ -36,7 +36,7 @@ let linkedinLink4 = 'https://www.linkedin.com/in/poonyavee-wongwisetsuk-9a019725
 
 let memberName5 = 'Pornsawalee K.';
 let memberRole5 = 'Chief Operating Officer';
-let memberInfo5 = '3th year student KMUTT';
+let memberInfo5 = 'Backend Developer | Web Master';
 let memberImg5 = gus;
 let facebookLink5 = 'https://www.facebook.com/pornsawalee.kanjanatiti.7';
 let twitterLink5 = 'https://github.com/sugus-ss';
