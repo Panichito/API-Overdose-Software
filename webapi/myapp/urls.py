d703ee2b0a27f131f7a2cd4afabe6606187ed254
Panichito <panithi2001big@gmail.com>
2023-03-03 22:13:25 +0700

    delete todo

--- webapi/myapp/urls.py
+++ webapi/myapp/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     path('api/all-todolist/', all_todolist),
     path('api/post-todolist', post_todolist),
     path('api/update-todolist/<int:TID>', update_todolist),
+    path('api/delete-todolist/<int:TID>', delete_todolist),
 ]
